546e13296124d7d8319e63fb63a7dfa47f937088
Jason Munro <jmunro@lbl.gov>
2021-05-20 15:13:04 -0700

    Linting

--- setup.py
+++ setup.py
@@ -3,28 +3,25 @@ from numpy.distutils.core import setup, Extension
 
 
 extension_irr = Extension(
-    name = 'DiSPy.core.stokes',
-    extra_f90_compile_args=['-std=legacy'],
-    sources=['DiSPy/core/stokes.pyf','DiSPy/core/pir_dat_module.f90']
+    name="DiSPy.core.stokes",
+    extra_f90_compile_args=["-std=legacy"],
+    sources=["DiSPy/core/stokes.pyf", "DiSPy/core/pir_dat_module.f90"],
 )
 
 setup(
-    name='DiSPy',
-    version='0.2',
-    author='Jason M. Munro',
-    author_email='jmunro@lbl.gov',
+    name="DiSPy",
+    version="0.2",
+    author="Jason M. Munro",
+    author_email="jmunro@lbl.gov",
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),
-    package_dir={'DiSPy': 'DiSPy'},
-    package_data={'DiSPy': ['SPG_dict.txt', 'PIR_data.txt']},
-    scripts=['scripts/dispy'],
-    license='LICENSE.txt',
-    description='Utility to apply the distortion symmetry method.',
-    long_description=open('README.rst').read(),
-    provides=['dispy'],
-    requires=[
-        "numpy",
-        "spglib",
-        "pymatgen"],
-    python_requires='>=3.7',
+    package_dir={"DiSPy": "DiSPy"},
+    package_data={"DiSPy": ["SPG_dict.txt", "PIR_data.txt"]},
+    scripts=["scripts/dispy"],
+    license="LICENSE.txt",
+    description="Utility to apply the distortion symmetry method.",
+    long_description=open("README.rst").read(),
+    provides=["dispy"],
+    requires=["numpy", "spglib", "pymatgen"],
+    python_requires=">=3.7",
     ext_modules=[extension_irr],
 )
